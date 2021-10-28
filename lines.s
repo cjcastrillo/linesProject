90bde8636f4701551622ecf11ad64ffb5d03e4fb
Carlos Castrillo <cjcastrillo@cpp.edu>
2021-10-28 14:10:37 -0700

    Initial commit

---
+++ lines.s
@@ -0,0 +1,11 @@
+#
+#	Name:		Carlos Castrillo
+#	Project:	3
+#	Due:		10/29/2021
+#	Course:		cs-2640-02-f21
+#
+#	Description:
+#				Take user input text and output the array constructed
+#
+	.data
+	.text
