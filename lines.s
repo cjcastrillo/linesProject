a287ad73d63ed23296bd623ecc0db7563f1ae3ad
Carlos Castrillo <cjcastrillo@cpp.edu>
2021-10-29 14:29:20 -0700

    Added method headers

--- lines.s
+++ lines.s
@@ -8,4 +8,20 @@
 #				Take user input text and output the array constructed
 #
 	.data
+MAXLINES:
+	.word	10;
+LINELEN
+	.word	32;
+	
 	.text
+main:
+
+gets:
+
+puts:
+
+strlen:
+
+strdup:
+
+malloc:
